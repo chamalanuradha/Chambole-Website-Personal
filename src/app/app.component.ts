5269bd01add390747197a8549e534c04edde7968
chamalanuradha <19it458@itum.mrt.ac.lk>
2023-06-11 22:30:19 +0530

    login dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { MatDialog } from '@angular/material/dialog';
+import { LoginComponent } from './login/login.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(public dialog: MatDialog) {}
+
+
+  login() {
+    const dialogRef = this.dialog.open(LoginComponent);
+  }
 
 }
