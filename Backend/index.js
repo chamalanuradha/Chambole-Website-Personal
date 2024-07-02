ccc178c75e1549af79affd68c25c2d74dbd99d85
chamalanuradha <chamalanuradha98765@gmail.com>
2024-07-02 11:55:45 +0530

    set mongo db connection

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,22 @@
 const express = require("express");
-const mongose = require()
+const mongoose = require("mongoose");
 
+const app = express();
 
+// Middleware to parse JSON bodies
+app.use(express.json());
 
+// Database connection
+const DB_URL= 'mongodb+srv://chamal123:chamal123@user.8p0sd.mongodb.net/?retryWrites=true&w=majority&appName=user'
+mongoose.set('strictQuery', true);
+mongoose.connect(DB_URL)
+.then(()=>{
+  console.log("DB connected")
+})
+.catch((err) =>console.log("DB not connected",err));
 
-const app = express ();
+    // Set the port
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
